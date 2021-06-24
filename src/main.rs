63fac0b612a34ccc03a880b1a9b50fd1e7eb1be7
abishekk92 <bhat.abishek@gmail.com>
2021-06-24 20:23:21 +0530

    Set up config

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 use std::env;
 
+struct Opts {
+    input_file: String,
+    output_file: String,
+}
+
+impl Opts {
+    fn from_args(args: &Vec<String>) -> Opts {
+        let input_file = &args[1];
+        let output_file = &args[2];
+        return Opts {
+            input_file: input_file.to_string(),
+            output_file: output_file.to_string(),
+        };
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let opts: Opts = Opts::from_args(&args);
+    println!("{} {}", opts.input_file, opts.output_file)
 }
