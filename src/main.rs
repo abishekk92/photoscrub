2a2c1a8d59d6c6dcf7b64e6d2ebddb2e368627d8
abishekk92 <bhat.abishek@gmail.com>
2021-06-27 22:31:33 +0530

    Handle errors

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,27 @@ struct Opts {
 }
 
 impl Opts {
-    fn from_args(args: &Vec<String>) -> Opts {
+    fn from_args(args: &Vec<String>) -> Result<Opts, &str> {
+        if args.len() < 3 {
+            return Err("Commandline arguments missing, dunno what to do!");
+        }
         let input_file = &args[1];
         let output_file = &args[2];
-        return Opts {
+        return Ok(Opts {
             input_file: input_file.to_string(),
             output_file: output_file.to_string(),
-        };
+        });
     }
+}
+
+fn read_file(input_file: String) {
+    println!("Reading {}", input_file)
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let opts: Opts = Opts::from_args(&args);
-    println!("{} {}", opts.input_file, opts.output_file)
+    match Opts::from_args(&args) {
+        Ok(opts) => read_file(opts.input_file),
+        Err(error_str) => println!("Error:{}", error_str),
+    }
 }
