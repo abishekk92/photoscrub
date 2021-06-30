01e2d0851887933848febda28bd432e507118dd7
abishekk92 <bhat.abishek@gmail.com>
2021-06-30 12:46:49 +0530

    Add subcomamnds

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,61 @@
 use exif;
 use std::path::PathBuf;
+use std::str::FromStr;
+use std::string::ParseError;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
 struct Opts {
+    #[structopt(parse(from_os_str), short)]
     input_file: PathBuf,
-    output_file: PathBuf,
+    #[structopt(subcommand)]
+    cmd: Option<Command>,
+    #[structopt(parse(from_os_str), short)]
+    output_file: Option<PathBuf>,
+}
+
+#[derive(StructOpt, Debug)]
+enum Command {
+    List {
+        #[structopt(possible_values = &Filter::variants(), case_insensitive = true)]
+        filter: Filter,
+        #[structopt(short, long)]
+        show: bool,
+    },
+    Scrub {
+        #[structopt(possible_values = &Filter::variants(), case_insensitive = true)]
+        filter: Filter,
+    },
+    Overwrite {
+        #[structopt(possible_values = &Filter::variants(), case_insensitive = true)]
+        filter: Filter,
+    },
+}
+
+#[derive(StructOpt, Debug)]
+enum Filter {
+    All,
+    Device,
+    Geo,
+}
+
+impl FromStr for Filter {
+    type Err = ParseError;
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        match input.to_lowercase().as_ref() {
+            "all" => Ok(Filter::All),
+            "device" => Ok(Filter::Device),
+            "geo" => Ok(Filter::Geo),
+            // Returns all by default
+            _ => Ok(Filter::All),
+        }
+    }
+}
+
+impl Filter {
+    fn variants() -> [&'static str; 3] {
+        return ["all", "device", "geo"];
+    }
 }
 
 fn read_exif(path: PathBuf) {
