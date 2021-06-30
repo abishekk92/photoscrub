93a29e710b30a2ea33843316ee865338a7eb9465
abishekk92 <bhat.abishek@gmail.com>
2021-06-30 16:22:53 +0530

    Print list

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use exif;
 use std::path::PathBuf;
 use structopt::{clap::arg_enum, StructOpt};
 
+//TODO Figure out autogenerate of documentation
+
 #[derive(StructOpt, Debug)]
 struct Opts {
     #[structopt(parse(from_os_str), short)]
@@ -30,6 +32,8 @@ enum Command {
     },
 }
 
+//TODO Document which fields constitute as device ids vs geo ids.
+
 arg_enum! {
     #[derive(StructOpt, Debug)]
     enum Filter {
@@ -46,22 +50,36 @@ fn read_exif(path: PathBuf) -> Result<exif::Exif, exif::Error> {
     return exifreader.read_from_container(&mut bufreader);
 }
 
-fn print_metadata(exif: exif::Exif) {
+//TODO Find out a better way to group the parameters, filter and show belog together.
+fn print_metadata(exif: exif::Exif, filter: crate::Filter, show: bool) {
+    // Filter by options. all, device, geo
+    println!("Displaying {} fields", filter);
+    println!("\n========\n");
     for f in exif.fields() {
-        println!(
-            "{} {} {}",
-            f.tag,
-            f.ifd_num,
-            f.display_value().with_unit(&exif)
-        );
+        if show {
+            println!(
+                "{} {} {}",
+                f.tag,
+                f.ifd_num,
+                f.display_value().with_unit(&exif)
+            );
+        } else {
+            println!("{} {} ******", f.tag, f.ifd_num);
+        }
     }
+    println!("\n========\n");
 }
 
+// fn scrub(exif: exif::Exif) -> exif::Exif {}
+
+// fn overwrite(exif: exif::Exif) -> exif::Exif {}
 fn main() {
     let args = Opts::from_args();
     let exif = read_exif(args.input_file).expect("File not found");
-    println!("\nThe image contains following metadata");
-    println!("\n------\n");
-    print_metadata(exif);
-    println!("\n------\n");
+    match args.cmd {
+        Some(Command::List { filter, show }) => {
+            print_metadata(exif, filter, show);
+        }
+        _ => println!("Not supported yet"),
+    }
 }
