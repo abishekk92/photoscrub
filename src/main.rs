ef42bcf0a07c9c1d8c2c1972ae65bfdc3776e039
abishekk92 <bhat.abishek@gmail.com>
2021-07-02 17:21:03 +0530

    Figure out a way to write exif to file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused_variables)]
 use exif;
+use exif::experimental::Writer;
 use std::path::PathBuf;
 use structopt::{clap::arg_enum, StructOpt};
 
@@ -11,8 +12,6 @@ struct Opts {
     input_file: PathBuf,
     #[structopt(subcommand)]
     cmd: Option<Command>,
-    #[structopt(parse(from_os_str), short)]
-    output_file: Option<PathBuf>,
 }
 
 #[derive(StructOpt, Debug)]
@@ -28,10 +27,14 @@ enum Command {
     Scrub {
         #[structopt(possible_values = &Filter::variants(), case_insensitive = true)]
         filter: Filter,
+        #[structopt(parse(from_os_str), short)]
+        output_file: Option<PathBuf>,
     },
     Overwrite {
         #[structopt(possible_values = &Filter::variants(), case_insensitive = true)]
         filter: Filter,
+        #[structopt(parse(from_os_str), short)]
+        output_file: Option<PathBuf>,
     },
 }
 
@@ -53,60 +56,75 @@ fn read_exif(path: PathBuf) -> Result<exif::Exif, exif::Error> {
     return exifreader.read_from_container(&mut bufreader);
 }
 
-trait ExifData {
-    fn filter_by(self, filter: crate::Filter) -> Self;
-    fn print_metadata(self, show: bool);
-    fn scrub(self) -> Self;
-    fn overwrite(self) -> Self;
-}
-
-impl ExifData for exif::Exif {
-    fn filter_by(self, filter: crate::Filter) -> Self {
-        return self;
-    }
-    fn print_metadata(self, show: bool) {
-        // println!("Displaying {} fields", filter);
-        // let fields = self.fields().filter(|x| match filter {
-        //     crate::Filter::All => true,
-        //     crate::Filter::Geo => x.tag.to_string().contains("GPS"),
-        //     crate::Filter::Device => x.tag.to_string() == "Make" || x.tag.to_string() == "Model",
-        // });
-        println!("========");
-        for f in self.fields() {
-            if show {
-                println!(
-                    "{} {} {}",
-                    f.tag,
-                    f.ifd_num,
-                    f.display_value().with_unit(&self)
-                );
-            } else {
-                println!("{} {} ******", f.tag, f.ifd_num);
-            }
-        }
-        println!("========");
-    }
-    fn scrub(self) -> Self {
-        println!("Scrubbed fields");
-        return self;
-    }
-    fn overwrite(self) -> Self {
-        println!("Overwrite fields");
-        return self;
-    }
-}
-
 fn main() {
     let args = Opts::from_args();
     let exif = read_exif(args.input_file).expect("File not found");
 
     match args.cmd {
-        Some(Command::List { filter, show }) => exif.filter_by(filter).print_metadata(show),
+        Some(Command::List { filter, show }) => {
+            println!("Metadata");
+            let fields = exif.fields().filter(|x| match filter {
+                crate::Filter::All => true,
+                crate::Filter::Geo => x.tag.to_string().contains("GPS"),
+                crate::Filter::Device => {
+                    x.tag.to_string() == "Make" || x.tag.to_string() == "Model"
+                }
+            });
+            for f in fields {
+                if show {
+                    println!(
+                        "{} {} {}",
+                        f.tag,
+                        f.ifd_num,
+                        f.display_value().with_unit(())
+                    );
+                } else {
+                    println!("{} {} ******", f.tag, f.ifd_num);
+                }
+            }
+        }
         // Lack of support for keyword arguments make this function call look ugly
         // TODO Figure out a better way to do this.
-        Some(Command::Scrub { filter }) => exif.filter_by(filter).scrub().print_metadata(false),
-        Some(Command::Overwrite { filter }) => {
-            exif.filter_by(filter).overwrite().print_metadata(false)
+        Some(Command::Scrub {
+            filter,
+            output_file,
+        }) => {
+            println!("Metadata");
+            let fields = exif.fields().filter(|x| match filter {
+                crate::Filter::All => !true,
+                crate::Filter::Geo => !x.tag.to_string().contains("GPS"),
+                crate::Filter::Device => {
+                    !(x.tag.to_string() == "Make" || x.tag.to_string() == "Model")
+                }
+            });
+            let mut writer = Writer::new();
+            let mut buf = std::io::Cursor::new(Vec::new());
+            let mut count: i8 = 0;
+            for f in fields {
+                println!("Scrubbing {} {} ******", f.tag, f.ifd_num);
+                writer.push_field(&f);
+                count += 1;
+            }
+            if count > 0 {
+                writer.write(&mut buf, false).expect("asdfasfsa");
+                println!("{:?}", buf.into_inner())
+            }
+        }
+        Some(Command::Overwrite {
+            filter,
+            output_file,
+        }) => {
+            println!("Metadata");
+            let fields = exif.fields().filter(|x| match filter {
+                crate::Filter::All => true,
+                crate::Filter::Geo => x.tag.to_string().contains("GPS"),
+                crate::Filter::Device => {
+                    x.tag.to_string() == "Make" || x.tag.to_string() == "Model"
+                }
+            });
+            for f in fields {
+                println!("Overwriting {} {} ******", f.tag, f.ifd_num);
+            }
         }
         _ => println!("Not supported yet"),
     }
