085552d48e697be3c12ad5165d082b141f0ec7fd
abishekk92 <bhat.abishek@gmail.com>
2021-07-02 11:00:06 +0530

    Refactor with a Trait

--- src/main.rs
+++ src/main.rs
@@ -53,47 +53,61 @@ fn read_exif(path: PathBuf) -> Result<exif::Exif, exif::Error> {
     return exifreader.read_from_container(&mut bufreader);
 }
 
-//TODO Find out a better way to group the parameters, filter and show belong together.
-fn print_metadata(exif: exif::Exif, filter: crate::Filter, show: bool) {
-    println!("\n========");
-    println!("Displaying {} fields", filter);
-    println!("========\n");
-    let fields = exif.fields().filter(|x| match filter {
-        crate::Filter::All => true,
-        crate::Filter::Geo => x.tag.to_string().contains("GPS"),
-        crate::Filter::Device => x.tag.to_string() == "Make" || x.tag.to_string() == "Model",
-    });
-    for f in fields {
-        if show {
-            println!(
-                "{} {} {}",
-                f.tag,
-                f.ifd_num,
-                f.display_value().with_unit(&exif)
-            );
-        } else {
-            println!("{} {} ******", f.tag, f.ifd_num);
-        }
+trait ExifData {
+    fn filter_by(self, filter: crate::Filter) -> Self;
+    fn print_metadata(self, show: bool);
+    fn scrub(self) -> Self;
+    fn overwrite(self) -> Self;
+}
+
+impl ExifData for exif::Exif {
+    fn filter_by(self, filter: crate::Filter) -> Self {
+        return self;
     }
-}
-
-fn scrub(exif: exif::Exif, filter: crate::Filter) {
-    println!("Scrubbed {} fields", filter)
-}
-
-fn overwrite(exif: exif::Exif, filter: crate::Filter) {
-    println!("Overwriting {} successful", filter)
+    fn print_metadata(self, show: bool) {
+        // println!("Displaying {} fields", filter);
+        // let fields = self.fields().filter(|x| match filter {
+        //     crate::Filter::All => true,
+        //     crate::Filter::Geo => x.tag.to_string().contains("GPS"),
+        //     crate::Filter::Device => x.tag.to_string() == "Make" || x.tag.to_string() == "Model",
+        // });
+        println!("========");
+        for f in self.fields() {
+            if show {
+                println!(
+                    "{} {} {}",
+                    f.tag,
+                    f.ifd_num,
+                    f.display_value().with_unit(&self)
+                );
+            } else {
+                println!("{} {} ******", f.tag, f.ifd_num);
+            }
+        }
+        println!("========");
+    }
+    fn scrub(self) -> Self {
+        println!("Scrubbed fields");
+        return self;
+    }
+    fn overwrite(self) -> Self {
+        println!("Overwrite fields");
+        return self;
+    }
 }
 
 fn main() {
     let args = Opts::from_args();
     let exif = read_exif(args.input_file).expect("File not found");
+
     match args.cmd {
-        Some(Command::List { filter, show }) => {
-            print_metadata(exif, filter, show);
+        Some(Command::List { filter, show }) => exif.filter_by(filter).print_metadata(show),
+        // Lack of support for keyword arguments make this function call look ugly
+        // TODO Figure out a better way to do this.
+        Some(Command::Scrub { filter }) => exif.filter_by(filter).scrub().print_metadata(false),
+        Some(Command::Overwrite { filter }) => {
+            exif.filter_by(filter).overwrite().print_metadata(false)
         }
-        Some(Command::Scrub { filter }) => scrub(exif, filter),
-        Some(Command::Overwrite { filter }) => overwrite(exif, filter),
         _ => println!("Not supported yet"),
     }
 }
